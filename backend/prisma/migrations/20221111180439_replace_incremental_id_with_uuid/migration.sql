dc7fc92e884777ea07f0004b7554b6c9230e64f3
yunger <hello@yunger.dev>
2022-11-11 15:05:23 -0300

    chore(prisma): run migrations

---
+++ backend/prisma/migrations/20221111180439_replace_incremental_id_with_uuid/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterTable
+ALTER TABLE `Post` DROP PRIMARY KEY,
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    MODIFY `userId` VARCHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- AlterTable
+ALTER TABLE `RefreshToken` MODIFY `userId` VARCHAR(191) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `User` DROP PRIMARY KEY,
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`id`);
