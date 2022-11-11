0fc5d1e76f7c74e1f631c31341d0bd96c66902a7
yunger <hello@yunger.dev>
2022-11-11 14:42:40 -0300

    chore(backend): add utility functions

---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import type { User } from '@prisma/client';
+
+const JWT_ACCESS_SECRET = process.env['JWT_ACCESS_SECRET'];
+const JWT_REFRESH_SECRET = process.env['JWT_REFRESH_SECRET'];
+
+export const generateAccessToken = (user: User) => {
+  if (!JWT_ACCESS_SECRET) {
+    throw new Error('JWT_ACCESS_SECRET env variable is not defined');
+  }
+
+  return jwt.sign({ userId: user.id }, process.env['JWT_ACCESS_SECRET'] || '', {
+    expiresIn: '15m',
+  });
+};
+
+export const generateRefreshToken = (user: User, tokenId: string) => {
+  if (!JWT_REFRESH_SECRET) {
+    throw new Error('JWT_REFRESH_SECRET env variable is not defined');
+  }
+
+  return jwt.sign(
+    {
+      userId: user.id,
+      tokenId,
+    },
+    process.env.JWT_REFRESH_SECRET || '',
+    {
+      expiresIn: '7d',
+    }
+  );
+};
+
+export const generateTokens = (user: User, tokenId: string) => {
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user, tokenId);
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
