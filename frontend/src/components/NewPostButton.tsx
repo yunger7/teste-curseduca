4b468caf49d55578cdf8c2dd1601d45e1a25eb53
yunger <hello@yunger.dev>
2022-11-14 15:53:08 -0300

    chore(components): add NewPostButton component

--- frontend/src/components/NewPostButton.tsx
+++ frontend/src/components/NewPostButton.tsx
@@ -1,23 +1,45 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { Affix, Tooltip, ActionIcon } from '@mantine/core';
 import { TbPlus as IconNew } from 'react-icons/tb';
 
+import { PostModal, PostInfo } from './PostModal';
+import { createAxiosClient } from '../services/createAxiosClient';
+
+const client = createAxiosClient();
+
 export const NewPostButton = () => {
+  const [opened, setOpened] = useState(false);
+
+  async function handleSubmit({ title, content }: PostInfo) {
+    try {
+      await client.post('/posts', { title, content });
+    } catch (error) {
+      throw error;
+    }
+  }
+
   return (
-    <Affix position={{ bottom: 20, right: 20 }}>
-      <Tooltip label="Criar post">
-        <ActionIcon
-          component={Link}
-          size="xl"
-          color="grape"
-          variant="filled"
-          radius="md"
-          aria-label="Criar post"
-          to="/posts/new"
-        >
-          <IconNew size={24} />
-        </ActionIcon>
-      </Tooltip>
-    </Affix>
+    <>
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <Tooltip label="Criar post">
+          <ActionIcon
+            size="xl"
+            color="grape"
+            variant="filled"
+            radius="md"
+            aria-label="Criar post"
+            onClick={() => setOpened(true)}
+          >
+            <IconNew size={24} />
+          </ActionIcon>
+        </Tooltip>
+      </Affix>
+      <PostModal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        onSubmit={handleSubmit}
+        actionName="Criar post"
+      />
+    </>
   );
 };
