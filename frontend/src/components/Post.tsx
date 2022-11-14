db9c4572863d4f60fa4d40de385206c9c1fdbc41
yunger <hello@yunger.dev>
2022-11-14 19:11:57 -0300

    feat(post): add edit and delete functionality

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,5 +1,12 @@
+import { useState } from 'react';
 import { Card, Text, Badge, Group, Skeleton, ActionIcon } from '@mantine/core';
 import { TbEdit as IconEdit, TbTrash as IconDelete } from 'react-icons/tb';
+
+import { PostModal, PostInfo } from './PostModal';
+import { DeleteModal } from './DeleteModal';
+import { createAxiosClient } from '../services/createAxiosClient';
+
+const client = createAxiosClient();
 
 export type PostData = {
   id: string;
@@ -19,6 +26,25 @@ type PostProps = {
 };
 
 export const Post = ({ post, loading }: PostProps) => {
+  const [editModalOpened, setEditModalOpened] = useState(false);
+  const [deleteModalOpened, setDeleteModalOpened] = useState(false);
+
+  async function editPost(data: PostInfo) {
+    try {
+      await client.put(`/posts/${post?.id}`, data);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function deletePost() {
+    try {
+      await client.delete(`/posts/${post?.id}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
   if (loading) {
     return (
       <Card withBorder mb="xl">
@@ -32,35 +58,60 @@ export const Post = ({ post, loading }: PostProps) => {
 
   if (post) {
     return (
-      <Card withBorder mb="xl">
-        <Group>
-          <Text weight={600}>{post.title}</Text>
-          <Badge size="sm" variant="outline" mb={-2}>
-            {post.user.name}
-          </Badge>
-        </Group>
-        {post.content && <Text mb="xs">{post.content}</Text>}
-        <Group position="apart" spacing="xs" sx={{ alignItems: 'flex-end' }}>
-          <div>
-            <Text color="dimmed" size="sm">
-              Criado em: {new Date(post.createdAt).toLocaleString()}
-            </Text>
-            <Text color="dimmed" size="sm">
-              Atualizado em: {new Date(post.updatedAt).toLocaleString()}
-            </Text>
-          </div>
-          {localStorage.getItem('invo-user-id') === post.userId && (
-            <Group>
-              <ActionIcon color="grape" variant="outline">
-                <IconEdit size={18} />
-              </ActionIcon>
-              <ActionIcon color="grape" variant="outline">
-                <IconDelete size={18} />
-              </ActionIcon>
-            </Group>
-          )}
-        </Group>
-      </Card>
+      <>
+        <Card withBorder mb="xl">
+          <Group>
+            <Text weight={600}>{post.title}</Text>
+            <Badge size="sm" variant="outline" mb={-2}>
+              {post.user.name}
+            </Badge>
+          </Group>
+          {post.content && <Text mb="xs">{post.content}</Text>}
+          <Group position="apart" spacing="xs" sx={{ alignItems: 'flex-end' }}>
+            <div>
+              <Text color="dimmed" size="sm">
+                Criado em: {new Date(post.createdAt).toLocaleString()}
+              </Text>
+              <Text color="dimmed" size="sm">
+                Atualizado em: {new Date(post.updatedAt).toLocaleString()}
+              </Text>
+            </div>
+            {localStorage.getItem('invo-user-id') === post.userId && (
+              <Group>
+                <ActionIcon
+                  color="grape"
+                  variant="outline"
+                  onClick={() => setEditModalOpened(true)}
+                >
+                  <IconEdit size={18} />
+                </ActionIcon>
+                <ActionIcon
+                  color="grape"
+                  variant="outline"
+                  onClick={() => setDeleteModalOpened(true)}
+                >
+                  <IconDelete size={18} />
+                </ActionIcon>
+              </Group>
+            )}
+          </Group>
+        </Card>
+        <PostModal
+          opened={editModalOpened}
+          onClose={() => setEditModalOpened(false)}
+          onSubmit={editPost}
+          actionName="Editar post"
+          initialValue={{
+            title: post.title,
+            content: post.content,
+          }}
+        />
+        <DeleteModal
+          opened={deleteModalOpened}
+          onClose={() => setDeleteModalOpened(false)}
+          onConfirm={deletePost}
+        />
+      </>
     );
   }
 
